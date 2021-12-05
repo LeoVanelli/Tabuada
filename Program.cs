c163ba4095349298a66abd490ca9c851215d9aa3
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-12-05 13:19:03 -0300

    Adiciona atividade Tabuada

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=========== Tabuada ===========\n");
+
+            Console.Write("Tabuada do número: ");
+            int.TryParse(Console
+            .ReadLine()
+            .ToString(), out int _valor);
+
+            Console.WriteLine();
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{_valor} x {i} = {_valor*i}");
+            }
+        }
+    }
+}
